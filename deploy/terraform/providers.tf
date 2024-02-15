28ea3cf3dbca787829eb92ae8cb56d060a065976
Jairo Nascimento <jaironsousa@gmail.com>
2024-02-15 18:20:52 -0300

    Criação workflow tf_plan

--- deploy/terraform/providers.tf
+++ deploy/terraform/providers.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "4.52.0"
     }
   }
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "aprendizado"
 }
